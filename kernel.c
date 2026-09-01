fe44a84059acb5296aa3ce3b0c6350063e6ac1e5
quvi007 <quvi5052@gmail.com>
2026-09-01 00:49:20 -0400

    kernel panic!

--- kernel.c
+++ kernel.c
@@ -27,12 +27,13 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
     printf("Hello world length is %d\n", strlen("Hello world"));
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    
+    PANIC("booted!");
+    printf("this should not execute\n");
 }
 
 __attribute__((section(".text.boot")))
--- kernel.h
+++ kernel.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#define PANIC(fmt, ...)                                                         \
+    do {                                                                        \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);   \
+        while (true);                                                           \
+    } while (0);                                                                \
+
 struct sbiret {
     long error;
     long value;
