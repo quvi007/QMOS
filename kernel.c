377fd8800bebd66d15d96de51563f816caebe247
quvi007 <quvi5052@gmail.com>
2026-09-08 00:18:24 -0400

    malloc

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include "kernel.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
     register long a0 __asm__("a0") = arg0;
@@ -28,6 +29,19 @@ void handle_trap(struct trap_frame *f) {
     uint32_t stval = READ_CSR(stval);
     uint32_t user_pc = READ_CSR(sepc);
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
+}
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end) {
+        PANIC("out of memory");
+    }
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
 }
 
 __attribute__((naked))
@@ -111,11 +125,12 @@ void kernel_entry(void) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
     WRITE_CSR(stvec, (uint32_t) kernel_entry);
-    __asm__ __volatile__("unimp");
-    printf("Hello World!\n");
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 }
 
 __attribute__((section(".text.boot")))
